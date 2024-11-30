6efabeaf706bba899408632e4af5e1fa71084cc6
Damian Rosewarne <drosewarne@linz.govt.nz>
2024-11-30 13:11:47 +1300

    added env.production

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './App.css';
 import App from './App';
 
 // Import your clerk publishable key
-const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY ? process.env.REACT_APP_CLERK_PUBLISHABLE_KEY : "pk_test_ZXZvbHZlZC1hc3AtNTEuY2xlcmsuYWNjb3VudHMuZGV2JA"
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key")
