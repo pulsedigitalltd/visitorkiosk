db0ed83709151af53171a583f6e6be4418648aab
Damian Rosewarne <drosewarne@linz.govt.nz>
2025-01-31 09:34:16 +1300

    changed booking screen iframe link

--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -7,8 +7,7 @@ const BookPage = () => {
             <NavBar/>
             <h1 style={{ display: 'none' }}>Book Page</h1>
 
-            <iframe src="https://yournaturaldispensary.acuityscheduling.com" width="100%" height="100%"></iframe>
-            <script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
+            <iframe src="https://bookings.gettimely.com/yournaturaldispensary2/book?uri=https%3A%2F%2Fbook.gettimely.com%2FBooking%2FLocation%2F270666%3Fmobile%3DTrue%26params%3D%25253fclient-login%25253dtrue" width="100%" height="100%"></iframe>
         </div>
     );
 };
